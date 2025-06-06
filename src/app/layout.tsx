ca0509dc8b019684078f61e75696280a2ad62f91
sanatan-dive <sanatansharma352@gmail.com>
2025-06-06 12:38:04 +0530

    i fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
  
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <SidebarComponent isLoading={false}>
